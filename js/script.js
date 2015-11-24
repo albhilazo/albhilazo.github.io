a89cf317c56f229bd060ed54fe9278b0f32c9b0d
Albert Hilazo <albhilazo@gmail.com>
2015-11-24 21:24:29 +0100

    Footer copy email

--- js/script.js
+++ js/script.js
@@ -11,9 +11,39 @@ var showRandomLogo = function() {
     // Show random
     codeLogos[Math.floor(Math.random()*(codeLogos.length))]
         .classList.remove('hidden');
-}
+};
 
 Array.prototype.forEach.call(tabs, function(tab) {
     // Click event for every tab
     tab.addEventListener('click', showRandomLogo);
 });
+
+
+
+
+/* Email construction */
+var showEmailCopy = function(e) {
+    var em1 = 'alb';
+    var em2 = 'hilazo';
+    var domain = 'gmail';
+    var topdomain = 'com';
+
+    e.preventDefault();
+
+    // this = element with attached event
+    var emailCopy = this.relatedCopy;
+    if( emailCopy.classList.contains('hidden') ) {
+        // Show
+        emailCopy.classList.remove('hidden');
+        emailCopy.value = em1 + em2 + '@' + domain + '.' + topdomain;
+    } else {
+        // Hide
+        emailCopy.classList.add('hidden');
+        emailCopy.value = '';
+    }
+};
+
+// Footer email link
+var footerEmailShow = document.getElementById('footer-email-show');
+footerEmailShow.relatedCopy = document.getElementById('footer-email-copy');
+footerEmailShow.addEventListener('click', showEmailCopy);
